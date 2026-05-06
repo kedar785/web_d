479805becea06821f6edc20fb27ca86f3fb23141
kedar785 <baiskedar126@gmail.com>
2026-05-06 11:29:57 +0530

    lectuer1

---
+++ JavaScript/lec1.js
@@ -0,0 +1,19 @@
+ // console.log("hii")
+        // console.error("error")
+        // console.warn("hello")
+        var b=5
+        console.log(b)
+        console.log(5==5)
+        console.log(5=="5")
+        console.log(5+'5'); //"55"
+        console.log(5-'5');
+        console.log(false+5); //5
+        console.log(null+null);
+        console.log(undefined-1)
+        console.log(null+"hii")
+         console.log(1+2+"2")
+
+         //------------------
+         let score=100
+         let to=score==100
+          console.log(to)
