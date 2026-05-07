f76c59b48560d76b37d83680aab8aae79e043241
kedar785 <baiskedar126@gmail.com>
2026-05-07 11:29:49 +0530

    function

---
+++ JavaScript/lec2.js
@@ -0,0 +1,33 @@
+// HOISTING:-
+var a 
+console.log(a);
+a=5
+
+//---------------------
+// let a=5;
+// function sum(){
+//     let b=10
+//    // console.log(a);
+//     console.log(b);
+// }
+// sum()
+//   console.log(a); 
+//----------------------
+
+// console.log({}=={})
+//---------------------
+// let student={
+//     id:1,
+//     name:"om"
+// }
+// console.log(student.id);
+//------------------------
+
+// let arr=[1,2,3,4,5,true,"string"]
+// console.log(arr[0]);
+// for(let a in arr){
+//     console.log(a,"innn")
+// }
+// for(let a of arr){
+//     console.log(a,"offf")
+// }
