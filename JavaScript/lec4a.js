9da252d0599ecf2ea7060954835b940887b5b75f
kedar785 <baiskedar126@gmail.com>
2026-05-11 20:24:35 +0530

    function

---
+++ JavaScript/lec4a.js
@@ -0,0 +1,9 @@
+console.log("hiiii");
+// let btn=document.querySelector("button")
+// let btn=document.querySelector("#one")
+let btn=document.querySelector(".two")
+let body=document.querySelector("body")
+btn.addEventListener("click",function(){
+    // console.log("hello");
+    body.style.backgroundColor="blue";
+})
