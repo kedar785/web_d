d302a69203b6ea76ffe6bb305ed723100b8021bb
kedar785 <baiskedar126@gmail.com>
2026-05-22 11:32:47 +0530

    first

---
+++ JavaScript/lec13.js
@@ -0,0 +1,22 @@
+// let obj={
+//     id:1,
+//     firstName:"om",
+//     lastName:"tiwari",
+//     fullName:function(city){
+//         console.log(this.firstName+  this.lastName);
+        
+//     }
+// }
+// let userOne={
+//     id:10,
+//     firstName:"chockroch ",
+//     lastName:"janta party",
+
+// }
+// //obj.fullName.call(userOne,"delhi");
+// obj.fullName.apply(userOne,["delhi",51])
+
+let arr=[1,2,3,4]
+console.log(Array.prototype);
+
+
