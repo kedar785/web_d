bc4292fc2158c3c282eda51a22654d64522c8c03
kedar785 <baiskedar126@gmail.com>
2026-05-24 20:31:04 +0530

    currency Converter

--- JavaScript/lec13.js
+++ JavaScript/lec13.js
@@ -16,7 +16,29 @@
 // //obj.fullName.call(userOne,"delhi");
 // obj.fullName.apply(userOne,["delhi",51])
 
-let arr=[1,2,3,4]
-console.log(Array.prototype);
+// let arr=[1,2,3,4]
+// console.log(Array.prototype);
+
+
+// let str=" js css"
+// console.log(str.length);
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str.trim());
+// console.log(str.includes("s"));
+// console.log(str.indexOf("j"));
+// console.log(str.substring(0,4));
+// console.log(str.substr(0,4));
+// console.log(str.split(""));
+
+
+let str="js js"
+console.log(str.replaceAll("j"," "));
+
+let ans=str.split("").reverse().join("")
+console.log(ans);
+console.log(str.replaceAll("js","html"));
+console.log(str.charAt(0));
+
 
 
