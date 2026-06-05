d1ab3eb7d5012f97206f3c4f6a81ff6ed0bd6461
kedar785 <baiskedar126@gmail.com>
2026-06-05 20:13:09 +0530

    use reduser

--- lec14/my-react-app/src/TodoList.jsx
+++ lec14/my-react-app/src/TodoList.jsx
@@ -104,19 +104,93 @@
 //----------------------------------------------------------
 
 
-import React, { useReducer } from "react";
+// import React, { useReducer } from "react";
+
+// const TodoList = () => {
+//   const [color, dispatch] = useReducer((state, action) => action, "white");
+
+//   return (
+//     <div style={{ backgroundColor: color, height: "100vh" }}>
+//       <button onClick={() => dispatch("red")}>Red</button>
+//       <button onClick={() => dispatch("gold")}>Gold</button>
+//       <button onClick={() => dispatch("blue")}>Blue</button>
+//       <button onClick={() => dispatch("green")}>Green</button>
+//     </div>
+//   );
+// };
+
+// export default TodoList;
+//----------------------------------------------------------------
+
+//  *****TODO LIST USE REDUSER***********
+import React,{useReducer} from 'react'
 
 const TodoList = () => {
-  const [color, dispatch] = useReducer((state, action) => action, "white");
+    let  intialData={
+        input:"",
+        todos:[],
+        index:null
+    }
+    function reduser(state,action){
+        if(action.type=="SET_INP"){
+            return{
+                ...state,
+                input:action.payload,
+                
+            }
+        }
+        else if(action.type=="ADD_TODO"){
+            return{
+                todos:[...state.todos,state.input]
+            }
+        }else if(action.type=="DELET_TODO"){
+            return{
+                ...state,
+                todos:state.todos.filter((_,id)=>{
+                    return id!==action.payload
+                })
+            }
+        }else if(action.type=="UPDATE_TODO"){
+            let updateTodos=[...state.todos]
+            updateTodos[state.index]=state.input
+            return{
+                ...state,
+                todos:updateTodos,
+                input:"",
+                index:null
+            }
+        }else if(action.type=="EDIT_TODO"){
+            return{
+                ...state,
+                input:state.todos[action.payload],
+                index:action.payload
+            }
+        }
 
+    }
+      let [state,disptach]=   useReducer(reduser,intialData)
+      function handleSubmit(){
+        if(state.index!==null){
+            disptach({type:"UPDATE_TODO"})
+        }else{
+            disptach({type:"ADD_TODO"})
+        }
+      }
   return (
-    <div style={{ backgroundColor: color, height: "100vh" }}>
-      <button onClick={() => dispatch("red")}>Red</button>
-      <button onClick={() => dispatch("gold")}>Gold</button>
-      <button onClick={() => dispatch("blue")}>Blue</button>
-      <button onClick={() => dispatch("green")}>Green</button>
+    <div>        
+        <input  name='input'  value={state.input}  onChange={(e)=>disptach({type:"SET_INP",payload:e.target.value})}/>
+        <button onClick={handleSubmit}>{state.index!==null?"update":"add"}</button>
+        {  
+            state.todos.map((a,b)=>{
+                return(<>
+                <h4>{a}</h4>
+                <button onClick={()=>disptach({type:"DELET_TODO",payload:b})}>delet</button>
+                <button onClick={()=>disptach({type:"EDIT_TODO",payload:b})}>edit</button>
+                </>)
+            })
+        }
     </div>
-  );
-};
+  )
+}
 
-export default TodoList;
+export default TodoList
