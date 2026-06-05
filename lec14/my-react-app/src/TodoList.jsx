0675f6c691472fd380a0de2dd38b85b8eb70af97
kedar785 <baiskedar126@gmail.com>
2026-06-05 09:50:34 +0530

    todo

--- lec14/my-react-app/src/TodoList.jsx
+++ lec14/my-react-app/src/TodoList.jsx
@@ -1,56 +1,120 @@
-import React, {useEffect, useState } from "react";
-import "./Todo.css";
-const TodoList = () => {
-   const [task, setTask] = useState("");
-  const [todos, setTodos] = useState(()=>{
-    let data=  localStorage.getItem("key")
-    if(data){
-        return JSON.parse(data)
-    }
-    return []
-  });
-  useEffect(()=>{
-    localStorage.setItem("key",JSON.stringify(todos))
+// import React, {useEffect, useState } from "react";
+// import "./Todo.css";
+// const TodoList = () => {
+//   const [task, setTask] = useState("");
+//   let [index,SetIndex]=useState(null)
+//   const [todos, setTodos] = useState(()=>{
+//     let data=  localStorage.getItem("key")
+//     if(data){
+//         return JSON.parse(data)
+//     }
+//     return []
+//   });
+//   useEffect(()=>{
+//     localStorage.setItem("key",JSON.stringify(todos))
 
-  },[todos])
+//   },[todos])
 
 
 
-  return (
-    <div className="container">
-      <h1>Todo List</h1>
+//   function edit(index){
+//     setTask(todos[index])
+//     SetIndex(index)
 
-      <div className="input-box">
-        <input
-          type="text"
-          name="task"
-          value={task}
-          placeholder="Enter a task"
-          onChange={(e)=>setTask(e.target.value)}
-  
-        />
+//   }
 
-<button onClick={()=>setTodos([...todos,task])}>add</button>
-      </div>
+
+//   function handleAorUpdate(){
+//     if(task.trim()==""){
+//         return;
+//     }
+//     console.log("helloooooo");
+    
+//     if(index!==null){
+//         let updateDATA=[...todos]
+//         updateDATA[index]=task
+//         setTodos(updateDATA)
+//     }else{
+//         setTodos([...todos,task])
+//         setTask("")
+//     }
     
 
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <div className="todo-item" key={index}>
-            <span>{todo}</span>
+//   }
 
-            <div className="actions">
-              <button >
-                Edit
-              </button>
 
-              <button >
-                Delete
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+//   function d(id){
+//    let d= todos.filter((a,b)=>{
+//         return id!=b
+
+//     })
+//     setTodos(d)
+
+//   }
+
+
+
+
+
+
+
+
+//   return (
+//     <div className="container">
+//       <h1>Todo List</h1>
+
+//       <div className="input-box">
+//         <input
+//           type="text"
+//           name="task"
+//           value={task}
+//           placeholder="Enter a task"
+//           onChange={(e)=>setTask(e.target.value)}
+  
+//         />
+
+// <button onClick={handleAorUpdate}>
+//     {index!==null?"update":"Add"}
+//     </button>
+//       </div>
+    
+
+//       <div className="todo-list">
+//         {todos.map((todo, index) => (
+//           <div className="todo-item" key={index}>
+//             <span>{todo}</span>
+
+//             <div className="actions">
+//               <button onClick={()=>edit(index)}>
+//                 Edit
+//               </button>
+
+//               <button  onClick={()=>d(index)}>
+//                 Delete
+//               </button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TodoList;
+//----------------------------------------------------------
+
+
+import React, { useReducer } from "react";
+
+const TodoList = () => {
+  const [color, dispatch] = useReducer((state, action) => action, "white");
+
+  return (
+    <div style={{ backgroundColor: color, height: "100vh" }}>
+      <button onClick={() => dispatch("red")}>Red</button>
+      <button onClick={() => dispatch("gold")}>Gold</button>
+      <button onClick={() => dispatch("blue")}>Blue</button>
+      <button onClick={() => dispatch("green")}>Green</button>
     </div>
   );
 };
